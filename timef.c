e92a0dc4a0930f20ee5a5aec49d1c1e506915ced
rad16 <herbivore.linux@gmail.com>
2015-10-22 19:33:04 -0400

    init timestamp commit

--- timef.c
+++ timef.c
@@ -3,14 +3,14 @@
 
 /* DEV file for rnote timestamp */
 
-	// This function will print date and time to a file
+	// This function will full date and time 
 	void stamp()
 {
         time_t time_raw_format;
 		struct tm * time_struct;
 
 		char sep[10] = "-------\n";
-		char *tstamp[100];
+		char tstamp[100];
 		time ( &time_raw_format );
 		time_struct = localtime ( &time_raw_format );
 	
@@ -21,7 +21,7 @@ int main ()
 	{   
 	    char ans[100], tstamp[100];
 	    printf("Hello baby! Give something to stamp!\n");
-	    	scanf("%[^n]", ans);
+	    	scanf("%[^\n]", ans);
 
 	   /*Print this message to file, prefix w/
 	   timestamp */
