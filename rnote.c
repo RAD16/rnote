7a2a45d399b775f09393a726ae459a93c9630baa
Ryan Donnelly <herbivore.linux@gmail.com>
2016-03-29 16:16:01 -0400

    deprecated alternate editor feature. Never used.

--- rnote.c
+++ rnote.c
@@ -7,7 +7,7 @@
 *
 */
 
-#define VERSION "rnote-0.1" 
+#define VERSION "rnote-0.2" 
 #define YEAR "2016" 
 
 #include <stdio.h>
@@ -84,10 +84,10 @@ get_dir(char *dir) {
 }
 
 void
-write_note(char *note, char *editor) {
+write_note(char *note) {
 	char com[50];
 	
-	snprintf(com, sizeof(com), "%s %s", editor, note);
+	snprintf(com, sizeof(com), "%s %s", EDITOR, note);
 	execl("/bin/sh", "sh", "-c", com, (char *)NULL);
 }
 
@@ -187,14 +187,6 @@ inline_note(char *file, size_t len, char *line) {
 	
 	/* Terminate resulting string dynamically */
 	title[i - ((n == 3) ? 1:0)] = '\0';
-/*	or
-	(n == 3) ? (title[i - 1] = '\0') : (title[i] = '\0');
-	or
-	if(n == 3)
-		title[i - 1] - '\0';
-	else
-		title[i] = '\0';
-*/
 
 	if(strlen(title) > sizeof(title)) {
 		puts("Title bonked, but we recorded your note!");
@@ -213,23 +205,18 @@ main(int argc, char *argv[]) {
 
 	if(argc == 1) {
 		get_filename(file, NULL);	
-		write_note(file, EDITOR);			
+		write_note(file);			
 
 	} else if(argc == 2 && strstr(argv[1], " ")) {
 		inline_note(file, sizeof(file), argv[1]);
 
 	} else if(argc == 2 && argv[1][0] != '-') {
 		get_filename(file, argv[1]);
-		write_note(file, EDITOR);			
+		write_note(file);			
 	} else if(argv[1][0] == '-') {
 
 		char opt = argv[1][1];
 		switch(opt) {
-			case 'e' :
-				argv[2] ?: die("Please specify an editor.");
-				get_filename(file, argv[3] ?: NULL);
-				write_note(file, argv[2]);			
-				break;
 			case 'l':
 				if(argv[2]) die("Option 'l' takes no arguments.");
 				list_notes();
